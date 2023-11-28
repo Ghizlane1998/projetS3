071a1761c133f3277f569d5c721b3adc6df41cff
Bennani Ghizlane <123105279+GJislaine@users.noreply.github.com>
2023-11-28 17:47:05 +0100

    Update Commentaire.java

--- src/main/java/entities/Commentaire.java
+++ src/main/java/entities/Commentaire.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 
 public class Commentaire {
 	private int id_commentaire;
+	private int id_acheteur;
+	private int id_article;
 	private String contenu;
 	private Date date;
 	
@@ -12,18 +14,32 @@ public class Commentaire {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public int getId_commentaire() {
-		return id_commentaire;
-	}
 	public Commentaire(int id_commentaire, String contenu, Date date) {
 			super();
 			this.id_commentaire = id_commentaire;
 			this.contenu = contenu;
 			this.date = date;
 		}
+	public int getId_commentaire() {
+		return id_commentaire;
+	}
 
 	public void setId_commentaire(int id_commentaire) {
 		this.id_commentaire = id_commentaire;
+	}
+	public int getId_acheteur() {
+		return id_acheteur;
+	}
+
+	public void setId_acheteur(int id_acheteur) {
+		this.id_acheteur = id_acheteur;
+	}
+	public int getId_article() {
+		return id_article;
+	}
+
+	public void setId_article(int id_article) {
+		this.id_article = id_article;
 	}
 	public String getContenu() {
 		return contenu;
